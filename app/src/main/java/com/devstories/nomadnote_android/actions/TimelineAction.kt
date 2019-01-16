e268daedba1fd075f45b496145ecb127693e27b8
dev3 <cj920406@naver.com>
2019-01-16 15:18:54 +0900

    진선 올림

--- app/src/main/java/com/devstories/nomadnote_android/actions/TimelineAction.kt
+++ app/src/main/java/com/devstories/nomadnote_android/actions/TimelineAction.kt
@@ -9,7 +9,17 @@ object TimelineAction {
 
     // 타임라인 작성
     fun addtimeline(params: RequestParams, handler: JsonHttpResponseHandler) {
-        HttpClient.post("api/timeline/addtimeline", params, handler)
+        HttpClient.post("/api/timeline/addtimeline", params, handler)
+    }
+
+    // 타임라인 가져오기
+    fun my_timeline(params: RequestParams, handler: JsonHttpResponseHandler) {
+        HttpClient.post("/api/timeline/my_timeline", params, handler)
+    }
+
+    // 타임라인 상세
+    fun detail_timeline(params: RequestParams, handler: JsonHttpResponseHandler) {
+        HttpClient.post("/api/timeline/detail_timeline", params, handler)
     }
 
 
--- app/src/main/java/com/devstories/nomadnote_android/activities/Solo_time_Fragment.kt
+++ app/src/main/java/com/devstories/nomadnote_android/activities/Solo_time_Fragment.kt
@@ -1,5 +1,7 @@
 package com.devstories.nomadnote_android.activities
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.app.ProgressDialog
 import android.content.Context
 import android.content.Intent
@@ -8,20 +10,47 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridView
 import com.devstories.nomadnote_android.R
+import com.devstories.nomadnote_android.actions.TimelineAction
+import com.devstories.nomadnote_android.base.PrefUtils
+import com.devstories.nomadnote_android.base.Utils
+import com.loopj.android.http.JsonHttpResponseHandler
+import com.loopj.android.http.RequestParams
+import cz.msebera.android.httpclient.Header
+import donggolf.android.adapters.SoloTimeAdapter
 import kotlinx.android.synthetic.main.fra_solo_time.*
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 
 class Solo_time_Fragment : Fragment()  {
     lateinit var myContext: Context
     private var progressDialog: ProgressDialog? = null
 
+    var timelineDatas:ArrayList<JSONObject> = ArrayList<JSONObject>()
+    lateinit var timelineAdaper:SoloTimeAdapter
+
+    lateinit var gridGV:GridView
+
+    var SOLO_WRITE = 1000
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         this.myContext = container!!.context
         progressDialog = ProgressDialog(myContext)
+
+        my_timeline()
+
         return inflater.inflate(R.layout.fra_solo_time, container, false)
 
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        gridGV = view.findViewById(R.id.gridGV)
+
+        timelineAdaper = SoloTimeAdapter(myContext!!,R.layout.item_solo_grid,timelineDatas)
+        gridGV.adapter = timelineAdaper
+
         super.onViewCreated(view, savedInstanceState)
     }
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -29,14 +58,149 @@ class Solo_time_Fragment : Fragment()  {
         click()
     }
     fun click(){
-        soloRL.setOnClickListener {
-            val intent = Intent(myContext, Solo_detail_Activity::class.java)
-            startActivity(intent)
-        }
+//        soloRL.setOnClickListener {
+//            val intent = Intent(myContext, Solo_detail_Activity::class.java)
+//            startActivity(intent)
+//        }
         writeRL.setOnClickListener {
             val intent = Intent(myContext, WriteActivity::class.java)
+            startActivityForResult(intent,SOLO_WRITE)
+        }
+
+        gridGV.setOnItemClickListener { parent, view, position, id ->
+            val timeline = timelineDatas.get(position)
+            val timeline_id = Utils.getString(timeline, "id")
+
+            val intent = Intent(myContext, Solo_detail_Activity::class.java)
+            intent.putExtra("timeline_id",timeline_id)
             startActivity(intent)
         }
+    }
+
+    fun my_timeline(){
+        val params = RequestParams()
+//        params.put("member_id", PrefUtils.getIntPreference(context,"member_id"))
+        params.put("member_id", "1")
+
+        TimelineAction.my_timeline(params, object : JsonHttpResponseHandler() {
+
+            override fun onSuccess(statusCode: Int, headers: Array<Header>?, response: JSONObject?) {
+                if (progressDialog != null) {
+                    progressDialog!!.dismiss()
+                }
+
+                try {
+
+                    val result =   Utils.getString(response,"result")
+                    if ("ok" == result) {
+                        if (timelineDatas != null){
+                            timelineDatas.clear()
+                        }
+
+                        val datas = response!!.getJSONArray("timeline")
+                        if (datas.length() > 0){
+                            for (i in 0 until datas.length()){
+                                val timeline = datas.get(i) as JSONObject
+                                timelineDatas.add(timeline)
+                            }
+                        }
+                        timelineAdaper.notifyDataSetChanged()
+                    }
+
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                }
+
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Array<Header>?, response: JSONArray?) {
+                super.onSuccess(statusCode, headers, response)
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Array<Header>?, responseString: String?) {
+
+                // System.out.println(responseString);
+            }
+
+            private fun error() {
+                Utils.alert(context, "조회중 장애가 발생하였습니다.")
+            }
+
+            override fun onFailure(
+                    statusCode: Int,
+                    headers: Array<Header>?,
+                    responseString: String?,
+                    throwable: Throwable
+            ) {
+                if (progressDialog != null) {
+                    progressDialog!!.dismiss()
+                }
+
+                // System.out.println(responseString);
+
+                throwable.printStackTrace()
+                error()
+            }
+
+            override fun onFailure(
+                    statusCode: Int,
+                    headers: Array<Header>?,
+                    throwable: Throwable,
+                    errorResponse: JSONObject?
+            ) {
+                if (progressDialog != null) {
+                    progressDialog!!.dismiss()
+                }
+                throwable.printStackTrace()
+                error()
+            }
+
+            override fun onFailure(
+                    statusCode: Int,
+                    headers: Array<Header>?,
+                    throwable: Throwable,
+                    errorResponse: JSONArray?
+            ) {
+                if (progressDialog != null) {
+                    progressDialog!!.dismiss()
+                }
+                throwable.printStackTrace()
+                error()
+            }
+
+            override fun onStart() {
+                // show dialog
+                if (progressDialog != null) {
+
+                    progressDialog!!.show()
+                }
+            }
+
+            override fun onFinish() {
+                if (progressDialog != null) {
+                    progressDialog!!.dismiss()
+                }
+            }
+        })
+
+
+    }
+
+    @SuppressLint("NewApi")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+
+            when (requestCode) {
+                SOLO_WRITE -> {
+                    if (data!!.getStringExtra("reset") != null){
+                       my_timeline()
+                    }
+                }
+            }
+        }
+
     }
 
 
