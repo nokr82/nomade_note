6026729be680871768f2347cc47a74b0eaa11540
youngju <youngjucha0316@gmail.com>
2019-01-16 10:43:40 +0900

    동우등록

---
+++ app/src/main/java/com/devstories/nomadnote_android/actions/LoginAction.kt
@@ -0,0 +1,16 @@
+package com.devstories.nomadnote_android.actions
+
+import com.devstories.nomadnote_android.base.HttpClient
+import com.loopj.android.http.JsonHttpResponseHandler
+import com.loopj.android.http.RequestParams
+
+
+object LoginAction {
+
+    // 핸드폰 인증
+    fun login(params: RequestParams, handler: JsonHttpResponseHandler) {
+        HttpClient.post("/api/login/login", params, handler)
+    }
+
+
+}
