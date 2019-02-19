58bf846d69f7a86e3c9972bc57f9e11db42c864d
dev3 <cj920406@naver.com>
2019-02-19 10:31:31 +0900

    진선 올림

--- app/src/main/java/com/devstories/nomadnote_android/activities/LoginActivity.kt
+++ app/src/main/java/com/devstories/nomadnote_android/activities/LoginActivity.kt
@@ -135,7 +135,7 @@ class LoginActivity : FragmentActivity(), GoogleApiClient.OnConnectionFailedList
         userManagement = UserManagement.getInstance()
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-//                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build()
         /* mGoogleApiClient = GoogleApiClient.Builder(this)
